8c24c70c5f78cf2a1f34a5f5e4e90d8c6c6b2f36
Manu <manuellp87@gmail.com>
2020-03-05 15:35:06 +0100

    Termina apartado 15

--- 9. news/articles/views.py
+++ 9. news/articles/views.py
@@ -3,17 +3,21 @@ from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from . import models
 
 
 class ArticleListView(ListView):
     model = models.Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
 
 class ArticleDetailView(DetailView):
     model = models.Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
 
 class ArticleUpdateView(UpdateView):
@@ -21,15 +25,23 @@ class ArticleUpdateView(UpdateView):
     fields = ['title', 'body', ]
     template_name = 'article_edit.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
 
 class ArticleDeleteView(DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = models.Article
     template_name = 'article_new.html'
-    fields = ['title', 'body', 'author', ]
+    success_url = reverse_lazy('article_list')
+    fields = ['title', 'body', ]
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
